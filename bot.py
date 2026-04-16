5625a1624cbf3835adc6411f2da652d442687fb5
srvsaklani-pixel <srvsaklani@gmail.com>
2026-04-16 06:04:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,4 @@
-import yfinance as yf
-import pandas as pd
-import requests
 import os
 
-BOT_TOKEN = os.getenv("8693861675:AAH20sGC3PU_ehueIVTLT73UwwVHTCl4uxQ")
-CHAT_ID = os.getenv("5067510130")
-
-def send(msg):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
-
-# ✅ SIMPLE TEST (no loops, no indentation issues)
-msg = "🚀 TEST MESSAGE FROM BOT"
-send(msg)
-
-print("Message sent")
+print("TOKEN:", os.getenv("8693861675:AAH20sGC3PU_ehueIVTLT73UwwVHTCl4uxQ"))
+print("CHAT:", os.getenv("5067510130"))
