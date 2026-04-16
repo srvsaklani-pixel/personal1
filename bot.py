de826318e39734fac2a2e54746a7669df92d30a2
srvsaklani-pixel <srvsaklani@gmail.com>
2026-04-16 06:18:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,3 +2,15 @@ import os
 
 print("TOKEN:", os.getenv("8693861675:AAH20sGC3PU_ehueIVTLT73UwwVHTCl4uxQ"))
 print("CHAT:", os.getenv("5067510130"))
+
+# TEMP TEST
+BOT_TOKEN = "PASTE_YOUR_TOKEN_HERE"
+CHAT_ID = "PASTE_YOUR_CHAT_ID_HERE"
+
+import requests
+
+def send(msg):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+
+send("🚀 DIRECT TEST MESSAGE")
